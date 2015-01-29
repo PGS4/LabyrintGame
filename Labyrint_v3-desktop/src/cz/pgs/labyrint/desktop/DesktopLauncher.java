0c57f11b4b961dd788f11c3cdfe8b3fade0e6c97
Kecicz <mp.lp.gamegroup@gmail.com>
2015-01-29 22:37:54 +0100

    initial commit

---
+++ Labyrint_v3-desktop/src/cz/pgs/labyrint/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package cz.pgs.labyrint.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import cz.pgs.labyrint.AppMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AppMain(), config);
+	}
+}
